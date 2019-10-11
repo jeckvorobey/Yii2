eb319c3698f0afe0cc1908267886a967622c1f5a
jeckvorobey <jeckvorobey69@gmail.com>
2019-10-11 17:13:25 +0300

    save

--- lesson_5/controllers/RegistrationController.php
+++ lesson_5/controllers/RegistrationController.php
@@ -5,11 +5,17 @@ namespace app\controllers;
 
 
 use app\models\RegistrationForm;
+use Yii;
 use yii\web\Controller;
 
 class RegistrationController extends Controller
 {
     public function actionReg(){
-        return $this->render('reg', ['model' => new RegistrationForm()]);
+        $formModel = new RegistrationForm();
+        if($formModel->load(Yii::$app->request->post())){
+            print_r($formModel);
+        }
+
+        return $this->render('reg', ['model' => $formModel]);
     }
 }
